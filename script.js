9da5dca757b3326668ea96f0b57778b4cedfc063
sirharry77 <46300415+sirharry77@users.noreply.github.com>
2024-02-08 19:39:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -543,3 +543,14 @@ function handleHashChange() {
     }
 }
 
+var lastTouchEnd = 0;
+
+document.addEventListener('touchend', function (event) {
+    var now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+        event.preventDefault();
+    }
+    lastTouchEnd = now;
+});
+
+
